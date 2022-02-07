cded8fc977e1005d4a4c4e5a6b0e7debadcd956a
Mapletree <mapletree@Mapletrees-MacBook-Pro.local>
2022-02-07 23:32:31 +0800

    add error handler

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -12,25 +12,30 @@ class WorldTime {
 
   //async getTime function
   Future<void> getTime() async {
-    //use 'http' package to make a network request to get data
-    //uri = https://worldtimeapi.org/api/timezone/Asia/Manila
-    Response response = await get(Uri.http(
-        "worldtimeapi.org", "/api/timezone/$url")); //pass the url endpoint
-    //allows us to decode json string and format
-    Map data = jsonDecode(response.body);
+    //error handler
+    try {
+      //use 'http' package to make a network request to get data
+      //uri = https://worldtimeapi.org/api/timezone/Asia/Manila
+      Response response = await get(Uri.http(
+          "worldtimeapi.org", "/api/timezone/$url")); //pass the url endpoint
+      //allows us to decode json string and format
+      Map data = jsonDecode(response.body);
 
-    //get properties from data
-    //get the datetime property
-    String datetime = data["datetime"];
-    //get the offset property
-    String offset = data["utc_offset"];
+      //get properties from data
+      //get the datetime property
+      String datetime = data["datetime"];
+      //get the offset property
+      String offset = data["utc_offset"];
 
-    //create a datetime object
-    DateTime now = DateTime.parse(datetime.substring(0, 26));
+      //create a datetime object
+      DateTime now = DateTime.parse(datetime.substring(0, 26));
 
-    //override the declared time variable at the top
-    time = now.toString();
-
+      //override the declared time variable at the top
+      time = now.toString();
+    } catch (e) {
+      print("Caught error $e");
+      time = "Couldn't get time data";
+    }
   }
 }
 
